50105acbd7b215ed633ca5fb187599651983e0e1
Maycol David Lopez Largo <malopezla@unal.edu.co>
2025-09-22 09:49:16 -0500

    Update Reto_01.4.py

--- Reto-01/Reto_01.4.py
+++ Reto-01/Reto_01.4.py
@@ -1,12 +1,14 @@
-def mayor_suma(lista):
-    if len(lista) < 2:
+def mayor_suma(lista_nums):
+    if len(lista_nums) < 2:
         return None
-    mayor = lista[0] + lista[1]
-    for i in range(1, len(lista) - 1):
-        suma = lista[i] + lista[i + 1]
+    mayor = lista_nums[0] + lista_nums[1]
+    for i in range(1, len(lista_nums) - 1):
+        suma = lista_nums[i] + lista_nums[i + 1]
         if suma > mayor:
             mayor = suma
     return mayor
 
-lista = [1, 2, 3, 10, 8, 5, 11, 2]
-print("La mayor suma de dos números consecutivos es: ", mayor_suma(lista))
+entrada_usuario = input("Ingresa los números de la lista separados por comas: ")
+lista_nums = [int(numero) for numero in entrada_usuario.split(',')]   
+
+print("La mayor suma de dos números consecutivos es: ", mayor_suma(lista_nums))
