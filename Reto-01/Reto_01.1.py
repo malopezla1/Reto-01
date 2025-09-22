a8413af00e59a07ab478f4f77cc969ea95c4f5ea
Maycol David Lopez Largo <malopezla@unal.edu.co>
2025-09-22 09:46:44 -0500

    Update Reto_01.1.py

--- Reto-01/Reto_01.1.py
+++ Reto-01/Reto_01.1.py
@@ -1,4 +1,4 @@
-def operaciones(num1, num2, operation):
+def operaciones(num1: float, num2: float, operation: float):
     if operation == 1:
         return num1 + num2
     elif operation == 2:
@@ -11,8 +11,8 @@ def operaciones(num1, num2, operation):
         else:
             return "Error!: No se puede dividir por 0."
 
-num1: int = int(input("Ingresa tu primer número: "))
-num2: int = int(input("Ingresa tu segundo número: "))
+num1: float = int(input("Ingresa tu primer número: "))
+num2: float = int(input("Ingresa tu segundo número: "))
 print("\nSuma seleccione: 1 ")
 print("Resta seleccione: 2 ")
 print("Multiplicacion seleccione: 3 ")
@@ -20,4 +20,5 @@ print("Division seleccione: 4 ")
 operation = int(input("\nSelecciona el tipo de operación: "))
 
 print(operaciones(num1, num2, operation))
+
 
